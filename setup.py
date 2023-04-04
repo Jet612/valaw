f4d0ef04a290d8ee509f5e2bb69554390efd1003
JackAttack612 <jetflyer1130@gmail.com>
2023-04-04 02:01:16 -0400

    Account-V1
    Endpoints in https://developer.riotgames.com/apis#account-v1

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as readme:
+    long_desc = readme.read()
+
+setup(
+    name="valaw",
+    version="0.0.1",
+    author="Jet612",
+    description="An asynchronous API wrapper for VALORANT's API",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jet612/VALORANT-API-Wrapper",
+    project_urls={
+        "Documentation": "https://github.com/Jet612/VALORANT-API-Wrapper/tree/main/docs",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+                    'aiohttp==3.8.3',
+                    'setuptools==65.5.0'
+                    ]
+)
---
+++ src/valaw/client.py
@@ -0,0 +1,127 @@
+### Imports ###
+import aiohttp
+import json
+
+### Variables ###
+regions = ["ap", "br", "esports", "eu", "kr", "latam", "na"]
+clusters = ["americas", "asia", "esports", "europe"]
+
+### Custom Exceptions ###
+class Exceptions:
+    class InvalidCluster(ValueError):
+        """Invalid Cluster."""
+    class InvalidRegion(ValueError):
+        """Invalid Region."""
+
+### Content Verify ###
+async def verify_content(response):
+    """
+    Helper function to verify response content-type & deal
+    with the response appropriately 
+    """
+    content_type = response.headers.get("Content-Type")
+    if content_type == "application/json; charset=utf-8" or content_type == "application/json":
+        return await response.json()
+    elif content_type == "text/plain; charset=utf-8" or content_type == "text/plain":
+        return json.loads(await response.text())
+
+### Client ###
+class Client:
+    def __init__(self, token: str, cluster: str):
+        """Initialize the client."""
+
+        # Checking if the arguments are valid
+        if cluster.lower() not in clusters:
+            raise Exceptions.InvalidCluster(f"Invalid cluster, valid clusters are: {clusters}.")
+        
+        self.token = token
+        self.cluster = cluster
+
+        # Subclasses
+        self.account = Account(token, cluster)
+
+class Account():
+    def __init__(self, token: str, cluster: str):
+        self.token = token
+        self.cluster = cluster
+
+    async def by_puuid(self, puuid: str, cluster: str = None):
+        """Get account by PUUID."""
+
+        if cluster != None:
+            if cluster.lower() not in clusters:
+                raise Exceptions.InvalidCluster(f"Invalid cluster, valid clusters are: {clusters}.")
+        else:
+            cluster = self.cluster 
+
+        async with aiohttp.ClientSession() as session:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": self.token
+            }
+            async with session.get(f"https://{cluster}.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}", headers=headers) as resp:
+                return await verify_content(resp)
+            
+    async def by_riot_id(self, game_name: str, tag_line: str, cluster: str = None):
+        """Get account by Riot ID."""
+
+        if cluster != None:
+            if cluster.lower() not in clusters:
+                raise Exceptions.InvalidCluster(f"Invalid cluster, valid clusters are: {clusters}.")
+        else:
+            cluster = self.cluster 
+
+        async with aiohttp.ClientSession() as session:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": self.token
+            }
+            async with session.get(f"https://{cluster}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}", headers=headers) as resp:
+                return await verify_content(resp)
+            
+    async def me(self, authorization: str, cluster: str = None):
+        """Get account by access token."""
+
+        if cluster != None:
+            if cluster.lower() not in clusters:
+                raise Exceptions.InvalidCluster(f"Invalid cluster, valid clusters are: {clusters}.")
+        else:
+            cluster = self.cluster 
+
+        async with aiohttp.ClientSession() as session:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": self.token,
+                "Authorization": authorization
+            }
+            async with session.get(f"https://{cluster}.api.riotgames.com/riot/account/v1/accounts/me", headers=headers) as resp:
+                return await verify_content(resp)
+            
+    async def active_shards(self, puuid: str, cluster: str = None):
+        """Get active shard for a player."""
+
+        if cluster != None:
+            if cluster.lower() not in clusters:
+                raise Exceptions.InvalidCluster(f"Invalid cluster, valid clusters are: {clusters}.")
+        else:
+            cluster = self.cluster 
+
+        async with aiohttp.ClientSession() as session:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": self.token
+            }
+            async with session.get(f"https://{cluster}.api.riotgames.com/riot/account/v1/active-shards/by-game/val/by-puuid/{puuid}", headers=headers) as resp:
+                return await verify_content(resp)
