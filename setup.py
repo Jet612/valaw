8f63dbd8abd85a5a5c1f1a756316c7b5b6832f16
JackAttack612 <jetflyer1130@gmail.com>
2023-04-08 18:18:52 -0500

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="valaw",
-    version="0.0.2",
+    version="0.0.3",
     author="Jet612",
     description="An asynchronous API wrapper for VALORANT's API",
     long_description=long_desc,
