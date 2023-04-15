0c2640f33a183b3b3ec678178d7f85621822198b
Jet612 <jetflyer1130@gmail.com>
2023-04-15 00:23:26 -0400

    0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'valaw'
 copyright = '2023, Jet612'
 author = 'Jet612'
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
