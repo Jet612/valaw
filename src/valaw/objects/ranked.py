4896bcc4287d562677cb4ac139cecc1d96c3cc06
Jet612 <jetflyer1130@gmail.com>
2023-08-01 14:07:26 -0400

    Update ranked.py

--- src/valaw/objects/ranked.py
+++ src/valaw/objects/ranked.py
@@ -3,13 +3,13 @@ from typing import List, Optional
 
 @dataclass
 class PlayerDto:
-    puuid: str = ""
-    gameName: str = "Private"
-    tagLine: str = ""
     leaderboardRank: int
     rankedRating: int
     numberOfWins: int
     competitiveTier: int
+    puuid: str = ""
+    gameName: str = "Private"
+    tagLine: str = ""
 
 @dataclass
 class DetailsDto:
