43e4f11b8a56891978e2fc83e70ae4d751517436
Jackson Tavares <jetflyer1130@gmail.com>
2024-03-13 14:57:14 -0400

    Getting rid of ErrorDto

--- src/valaw/client.py
+++ src/valaw/client.py
@@ -9,14 +9,17 @@ from .objects.match import MatchDto, MatchlistDto, RecentMatchesDto
 from .objects.ranked import LeaderboardDto
 from .objects.status import PlatformDataDto
 from .objects.content import ContentDto
-from .objects.error import ErrorDto
 
 
 ### Variables ###
 regions = ["ap", "br", "esports", "eu", "kr", "latam", "na"]
+"""List of valid regions."""
 clusters = ["americas", "asia", "esports", "europe"]
+"""List of valid clusters."""
 locales = ['ar-ae', 'de-de', 'en-gb', 'en-us', 'es-es', 'es-mx', 'fr-fr', 'id-id', 'it-it', 'ja-jp', 'ko-kr', 'pl-pl', 'pt-br', 'ru-ru', 'th-th', 'tr-tr', 'vi-vn', 'zh-cn', 'zh-tw']
-queues = ["competitive", "unrated", "spikerush", "tournamentmode", "deathmatch", "onefa", "ggteam"]
+"""List of valid locales."""
+queues = ["competitive", "unrated", "spikerush", "tournamentmode", "deathmatch", "onefa", "ggteam", "hurm"]
+"""List of valid queues."""
 
 ### Custom Exceptions ###
 class Exceptions:
@@ -31,6 +34,8 @@ class Exceptions:
             self.status_message = status_message
             self.message = str(status_code) + " - " + status_message
             super().__init__(self.message)
+    class FailedToParseJSON(Exception):
+        """Failed to parse JSON."""
 
 ### Content Verify ###
 async def verify_content(response: aiohttp.ClientResponse):
@@ -43,6 +48,8 @@ async def verify_content(response: aiohttp.ClientResponse):
         return await response.json()
     elif content_type == "text/plain; charset=utf-8" or content_type == "text/plain" or content_type == "text/plain;charset=utf-8":
         return json.loads(await response.text())
+    else:
+        raise Exceptions.FailedToParseJSON(f"Failed to parse JSON, content-type: {content_type}.")
 
 ### Client ###
 class Client:
@@ -56,7 +63,6 @@ class Client:
         self.token = token
         self.cluster = cluster
         self.raw_data = raw_data
-        self.errors = errors
 
     def change_cluster(self, cluster: str):
         """Change the cluster."""
@@ -74,8 +80,16 @@ class Client:
     ### ACCOUNT-V1 ###
     ##################
 
-    async def GET_getByPuuid(self, puuid: str, cluster: str = None) -> Union[AccountDto, ErrorDto, Dict]:
-        """Get account by PUUID."""
+    async def GET_getByPuuid(self, puuid: str, cluster: str = None) -> Union[AccountDto, Dict]:
+        """
+        Get account by PUUID.
+        
+        :param puuid: The PUUID of the account.
+        :type puuid: str
+        :param cluster: The cluster to retreive from, defaults to self.cluster.
+        :type cluster: str
+        :rtype: Union[AccountDto, Dict]
+        """
 
         if cluster != None:
             if cluster.lower() not in clusters:
@@ -98,15 +112,21 @@ class Client:
                 
                 # Checking if the response is an error, then returning the appropriate object/exception
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(AccountDto, raw_response)
             
-    async def GET_getByRiotId(self, gameName: str, tagLine: str, cluster: str = None) -> Union[AccountDto, ErrorDto, Dict]:
-        """Get account by Riot ID."""
+    async def GET_getByRiotId(self, gameName: str, tagLine: str, cluster: str = None) -> Union[AccountDto, Dict]:
+        """Get account by Riot ID.
+        
+        :param gameName: The game name of the account (gameName#tagLine).
+        :type gameName: str
+        :param tagLine: The tag line of the account (gameName#tagLine).
+        :type tagLine: str
+        :param cluster: The cluster to retreive from, defaults to self.cluster.
+        :type cluster: str
+        :rtype: Union[AccountDto, Dict]
+        """
 
         if cluster != None:
             if cluster.lower() not in clusters:
@@ -127,15 +147,19 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(AccountDto, raw_response)
             
-    async def GET_getByAccessToken(self, authorization: str, cluster: str = None) -> Union[AccountDto, ErrorDto, Dict]:
-        """Get account by access token."""
+    async def GET_getByAccessToken(self, authorization: str, cluster: str = None) -> Union[AccountDto, Dict]:
+        """Get account by access token.
+        
+        :param authorization: The access token.
+        :type authorization: str
+        :param cluster: The cluster to retreive from, defaults to self.cluster.
+        :type cluster: str
+        :rtype: Union[AccountDto, Dict]
+        """
 
         if cluster != None:
             if cluster.lower() not in clusters:
@@ -157,15 +181,19 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(AccountDto, raw_response)
             
-    async def GET_getActiveShard(self, puuid: str, cluster: str = None) -> Union[ActiveShardDto, ErrorDto, Dict]:
-        """Get active shard for a player."""
+    async def GET_getActiveShard(self, puuid: str, cluster: str = None) -> Union[ActiveShardDto, Dict]:
+        """Get active shard for a player.
+        
+        :param puuid: The PUUID of the account.
+        :type puuid: str
+        :param cluster: The cluster to retreive from, defaults to self.cluster.
+        :type cluster: str
+        :rtype: Union[ActiveShardDto, Dict]
+        """
 
         if cluster != None:
             if cluster.lower() not in clusters:
@@ -186,10 +214,7 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(ActiveShardDto, raw_response)
 
@@ -197,8 +222,15 @@ class Client:
     ### VAL-CONTENT-V1 ###
     ######################
 
-    async def GET_getContent(self, region: str, locale: str = "") -> Union[ContentDto, ErrorDto, Dict]:
-        """Get content optionally filtered by locale. A locale is recommended to be used for faster response times."""
+    async def GET_getContent(self, region: str, locale: str = "") -> Union[ContentDto, Dict]:
+        """Get content optionally filtered by locale. A locale is recommended to be used for faster response times.
+        
+        :param region: The region to execute against.
+        :type region: str
+        :param locale: The locale to retrieve data for, defaults to "".
+        :type locale: str
+        :rtype: Union[ContentDto, Dict]
+        """
 
         if region.lower() not in regions:
             raise Exceptions.InvalidRegion(f"Invalid region, valid regions are: {regions}.")
@@ -221,10 +253,7 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(ContentDto, raw_response)
 
@@ -232,8 +261,15 @@ class Client:
     ### VAL-MATCH-V1 ###
     #################### 
 
-    async def GET_getMatch(self, matchId: str, region: str) -> Union[MatchDto, ErrorDto, Dict]:
-        """Get match by id."""
+    async def GET_getMatch(self, matchId: str, region: str) -> Union[MatchDto, Dict]:
+        """Get match by id.
+        
+        :param matchId: The match id.
+        :type matchId: str
+        :param region: The region to execute against.
+        :type region: str
+        :rtype: Union[MatchDto, Dict]
+        """
 
         if region.lower() not in regions:
             raise Exceptions.InvalidRegion(f"Invalid region, valid regions are: {regions}.")
@@ -251,15 +287,19 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(MatchDto, raw_response)
             
-    async def GET_getMatchlist(self, puuid: str, region: str) -> Union[MatchlistDto, ErrorDto, Dict]:
-        """Get matchlist for games played by puuid."""
+    async def GET_getMatchlist(self, puuid: str, region: str) -> Union[MatchlistDto, Dict]:
+        """Get matchlist for games played by puuid.
+        
+        :param puuid: The PUUID of the account.
+        :type puuid: str
+        :param region: The region to execute against.
+        :type region: str
+        :rtype: Union[MatchlistDto, Dict]
+        """
 
         if region.lower() not in regions:
             raise Exceptions.InvalidRegion(f"Invalid region, valid regions are: {regions}.")
@@ -277,16 +317,12 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(MatchlistDto, raw_response)
             
-    async def GET_getRecent(self, queue: str, region: str) -> Union[RecentMatchesDto, ErrorDto, Dict]:
-        """
-        Get recent matches.
+    async def GET_getRecent(self, queue: str, region: str) -> Union[RecentMatchesDto, Dict]:
+        """Get recent matches.
 
         Returns a list of match ids that have completed 
         in the last 10 minutes for live regions and 12 hours 
@@ -296,6 +332,12 @@ class Client:
         three regions. Requests are load balanced so you may 
         see some inconsistencies as matches are added/removed 
         from the list.
+
+        :param queue: The queue to retrieve recent matches for.
+        :type queue: str
+        :param region: The region to execute against.
+        :type region: str
+        :rtype: Union[RecentMatchesDto, Dict]
         """
 
         if region.lower() not in regions:
@@ -316,10 +358,7 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(RecentMatchesDto, raw_response)
     
@@ -327,8 +366,19 @@ class Client:
     ### VAL-RANKED-V1 ###
     #####################
 
-    async def GET_getLeaderboard(self, actId: str, region: str, size: int = 200, startIndex: int = 0) -> Union[LeaderboardDto, ErrorDto, Dict]:
-        """Get leaderboard for the competitive queue"""
+    async def GET_getLeaderboard(self, actId: str, region: str, size: int = 200, startIndex: int = 0) -> Union[LeaderboardDto, Dict]:
+        """Get leaderboard for the competitive queue.
+        
+        :param actId: The act id.
+        :type actId: str
+        :param region: The region to execute against.
+        :type region: str
+        :param size: The amount of entries to retrieve, defaults to 200.
+        :type size: int
+        :param startIndex: The index to start from, defaults to 0.
+        :type startIndex: int
+        :rtype: Union[LeaderboardDto, Dict]
+        """
 
         if region.lower() not in regions:
             raise Exceptions.InvalidRegion(f"Invalid region, valid regions are: {regions}.")
@@ -349,10 +399,7 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(LeaderboardDto, raw_response)
 
@@ -360,8 +407,13 @@ class Client:
     ### VAL-STATUS-V1 ###
     #####################
 
-    async def GET_getPlatformData(self, region: str) -> Union[PlatformDataDto, ErrorDto, Dict]:
-        """Get VALORANT status for the given platform."""
+    async def GET_getPlatformData(self, region: str) -> Union[PlatformDataDto, Dict]:
+        """Get VALORANT status for the given platform.
+        
+        :param region: The region to execute against.
+        :type region: str
+        :rtype: Union[PlatformDataDto, Dict]
+        """
 
         if region.lower() not in regions:
             raise Exceptions.InvalidRegion(f"Invalid region, valid regions are: {regions}.")
@@ -379,9 +431,6 @@ class Client:
                 if self.raw_data == True:
                     return raw_response
                 if raw_response.get("status") != None:
-                    if self.errors == True:
-                        raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
-                    else:
-                        return fromdict(ErrorDto, raw_response)
+                    raise Exceptions.RiotAPIResponseError(raw_response["status"]["status_code"], raw_response["status"]["message"])
                 else:
                     return fromdict(PlatformDataDto, raw_response)
