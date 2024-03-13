78f1a414b77d5d75523a581089a6c797a02f523a
Jackson Tavares <jetflyer1130@gmail.com>
2024-03-13 15:48:46 -0400

    0.1.0

--- setup.py
+++ setup.py
@@ -3,25 +3,30 @@ from setuptools import setup, find_namespace_packages
 with open("README.md", "r") as readme:
     long_desc = readme.read()
 
+url = "https://github.com/Jet612/valaw"
+
 setup(
     name="valaw",
-    version="0.0.9",
+    version="0.1.0",
     author="Jet612",
     description="An asynchronous API wrapper for VALORANT's API",
     long_description=long_desc,
     long_description_content_type="text/markdown",
     url="https://github.com/Jet612/valaw",
     project_urls={
+        "Source": url,
         "Documentation": "https://valaw.readthedocs.io",
-        "Issue Tracker": "https://github.com/Jet612/valaw/issues",
+        "Issue Tracker": url + "/issues",
         "Chat/Support": "https://discord.gg/mVXpvunBbF",
     },
     classifiers=[
-        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers"
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
     ],
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
